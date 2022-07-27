db96a8bab4d413f28c106020e03968cbd024ee40
GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
2022-07-27 14:41:30 -0300

    criando a rota get no arquivo clientesController.js

---
+++ src/controllers/clientesController.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+const clientesController = (app) => {
+
+    app.get('/clientes', async (req, res) => {
+        try {
+            const todosClientes = await ClientesModel.pegaCliente()
+            res.json({
+                "clientes": todosClientes,
+                "erro": false
+            })
+        } catch (error) {
+            res.json({
+                "msg": error.message,
+                "erro": true
+            })
+        }
+    })
+
+    app.get('/clientes/email/:email', async (req, res) => {
+        const email = req.params.email
+        try {
+            const cliente = await ClientesModel.pegaUmClienteEmail(email)
+            res.json({
+                "cliente": cliente,
+                "erro": false
+            })
+        } catch (error) {
+            res.json({
+                "msg": error.message,
+                "erro": true
+            })
+        }
+    })
+
+}
+
+
+export default clientesController

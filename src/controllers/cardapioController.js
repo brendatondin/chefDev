558bc0a87a896f77ebfa151010f8a3d0ed44f93f
joao-pedro-de-moura <jpmouraj@gmail.com>
2022-07-27 17:19:30 -0300

    criado rota post no arquivo cardapioController.js

--- src/controllers/cardapioController.js
+++ src/controllers/cardapioController.js
@@ -21,9 +21,9 @@ const cardapioController = (app) => {
 app.get('/cardapio/codigo/:codigo', async (req, res) => {
     const codigo = req.params.codigo
     try {
-        const codigo = await CodigoModel.pegaUmCardapioCodigo(email)
+        const codigo = await CodigoModel.pegaUmCardapioCodigo(codigo)
         res.json({
-            "cliente": cliente,
+            "cardapio": codigo,
             "erro": false
         })
     } catch (error) {
@@ -34,7 +34,24 @@ app.get('/cardapio/codigo/:codigo', async (req, res) => {
     }
 })
 
+app.post('/cardapio', async (req, res) => {
+    const body = req.body
+    try {
+        const novoPrato = criaPrato(body.prato, body.codigo)
+        await CardapioModel.inserePrato(novoPrato)
+        res.json({
+            "msg": "Prato inserido com sucesso",
+            "Prato": novoPrato,
+            "erro": false
+        })
 
+    } catch (error) {
+        res.json({
+            "msg": error.message,
+            "erro": true
+        })
+    }
+})
 
 
 

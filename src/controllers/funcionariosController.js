53999096547ec1ccf1df675deaa75f7fa580abe5
Barbara Guimaraes <102765523+BarbaraGuimaraes21@users.noreply.github.com>
2022-07-28 11:48:00 -0300

    Barbara funcionarios (#46)
    * criação de rota get no arquivo funcionariosController.js
    
    * criado rota post no arquivo funcionariosController.js
    
    * criado rota delete no arquivo funcionariosController.js
    
    * criado rota put no arquivo funcionariosController.js
    
    * criado rota patch no arquivo funcionariosController.js
    
    * realizado indentação do código no arquivo funcionariosController.js

--- src/controllers/funcionariosController.js
+++ src/controllers/funcionariosController.js
@@ -33,64 +33,85 @@ const funcionariosController = (app) => {
             })
         }
     })
+
+    app.post('/funcionarios', async (req, res) => {
+        const body = req.body
+        try {
+            const novoFuncionario = criafuncionario(body.nome, body.email, body.cargo, body.salario, body.contato)
+            await funcionariosModel.insereFuncionario(novoCliente)
+            res.json({
+                "msg": "Funcionário inserido com sucesso",
+                "funcionario": novoFuncionario,
+                "erro": false
+            })
+
+        } catch (error) {
+            res.json({
+                "msg": error.message,
+                "erro": true
+            })
+        }
+    })
+
+    app.delete('/funcionario/id/:id', async (req, res) => {
+        const id = req.params.id
+        try {
+            await funcionariosModel.deletaFuncionario(id)
+
+            res.json({
+                "msg": "Funcionário deletado com sucesso",
+                "erro": false
+            })
+
+        } catch (error) {
+            res.json({
+                "msg": error.message,
+                "erro": true
+            })
+        }
+    })
+
+    app.put('/funcionario/id/:id', async (req, res) => {
+        const body = req.body
+        const id = req.params.id
+        try {
+            const funcionarioValidado = criaFuncionario(body.nome, body.email, body.cargo, body.salario, body.contato)
+            await funcionariosModel.atualizaFuncionario(id, funcionarioValidado)
+            res.json({
+                "msg": "Funcionários atualizado com sucesso",
+                "funcionario": funcionarioValidado,
+                "erro": false
+            })
+
+        } catch (error) {
+            res.json({
+                "msg": error.message,
+                "erro": true
+            })
+        }
+    })
+
+    app.patch('/funcionario/contato/id/:id', async (req, res) => {
+        const id = req.params.id
+        const body = req.body
+        try {
+            validaFuncionario(body.contato)
+            await funcionariosModel.atualizaFuncionario(id, {
+                "contato": body.contato
+            })
+            res.json({
+                "msg": "Contato atualizada",
+                "erro": false
+            })
+
+        } catch (error) {
+            res.json({
+                "msg": error.message,
+                "erro": true
+            })
+        }
+    })
 }
-
-app.post('/funcionarios', async (req, res) => {
-    const body = req.body
-    try {
-        const novoFuncionario = criafuncionario(body.nome, body.email, body.cargo, body.salario, body.contato)
-        await funcionariosModel.insereFuncionario(novoCliente)
-        res.json({
-            "msg": "Funcionário inserido com sucesso",
-            "funcionario": novoFuncionario,
-            "erro": false
-        })
-
-    } catch (error) {
-        res.json({
-            "msg": error.message,
-            "erro": true
-        })
-    }
-})
-
-app.delete('/funcionario/id/:id', async (req, res) => {
-    const id = req.params.id
-    try {
-        await funcionariosModel.deletaFuncionario(id)
-
-        res.json({
-            "msg": "Funcionário deletado com sucesso",
-            "erro": false
-        })
-
-    } catch (error) {
-        res.json({
-            "msg": error.message,
-            "erro": true
-        })
-    }
-})
-
-app.put('/funcionario/id/:id', async (req, res) => {
-    const body = req.body
-    const id = req.params.id
-    try {
-        const funcionarioValidado = criaFuncionario(body.nome, body.email, body.cargo, body.salario, body.contato)
-        await funcionariosModel.atualizaFuncionario(id, funcionarioValidado)
-        res.json({
-            "msg": "Funcionários atualizado com sucesso",
-            "funcionario": funcionarioValidado,
-            "erro": false
-        })
-
-    } catch (error) {
-        res.json({
-            "msg": error.message,
-            "erro": true
-        })
-    }
-})
 
 app.patch('/funcionario/contato/id/:id', async (req, res) => {
     const id = req.params.id

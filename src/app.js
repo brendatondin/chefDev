6fbe398c7d911086aa11f618c142aa54a59e15fe
GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
2022-07-27 23:11:36 -0300

    alterações na rota get na pasta clientesController.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 import express from "express";
 import cardapioController from "./Controller/controller.js"
+import clientesController from "./Controller/clientesController.js"
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
+
+
 cardapioController(app)
-
-
-
-
+clientesController(app)
 
 
 

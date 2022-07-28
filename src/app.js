6f152d7368319cd18e19e80de2b9094ca762ba0a
Henry Januário <primeirohenry@gmail.com>
2022-07-28 11:11:48 -0300

    servidor aberto e rota get respondendo

--- src/app.js
+++ src/app.js
@@ -1,20 +1,12 @@
 import express from "express";
+import reservasController from "./controllers/reservasController.js" 
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-
-
-
-
-
-
-
-
-
-
+reservasController(app);
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}/`)

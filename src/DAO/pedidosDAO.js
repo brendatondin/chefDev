fbaf79c3e0069be32c9f034ee9b168eb32edc9ec
brendatondin <bentotbrenda@gmail.com>
2022-07-29 00:55:38 -0300

    adicionado rota get pegaUmPedidoComanda no arquivo pedidosController.js e criato o metodo pegaUmPedidoComanda no arquivo pedidosDAO.js

--- src/DAO/pedidosDAO.js
+++ src/DAO/pedidosDAO.js
@@ -15,7 +15,20 @@ const pedidosDAO  = {
         })
     },
 
-
+    pegaUmPedidoComanda: (comanda)=>{
+        const PEGA_UM_PEDIDO = `
+        SELECT * FROM PEDIDOS
+        WHERE comanda = ?
+        `
+        return new Promise((resolve, reject)=>{
+            db.get(PEGA_UM_PEDIDO, comanda, (error, row)=>{
+                if(error)
+                    reject(error)
+                else
+                    resolve(row)
+            })
+        })
+    },
 }
 
 export default pedidosDAO

57777bb311601347b88206229f97e05194c49ac9
Barbara Guimaraes <102765523+BarbaraGuimaraes21@users.noreply.github.com>
2022-07-30 19:39:52 -0300

    Barbara funcionarios (#78)
    * criação de rota get no arquivo funcionariosController.js
    
    * criado rota post no arquivo funcionariosController.js
    
    * criado rota delete no arquivo funcionariosController.js
    
    * criado rota put no arquivo funcionariosController.js
    
    * criado rota patch no arquivo funcionariosController.js
    
    * realizado indentação do código no arquivo funcionariosController.js
    
    * criado e estruturado a model no arquivo funcionariosModel.js
    
    * aceito as alterações de outras branches
    
    * criado código funcionariosModel.js e recuperado código do database create-and-populate.js
    
    * criado arquivo DAO e método pegaFuncionario - arquivo funcionariosDao.js
    
    * criado método pegaUmFuncionarioContato no arquivo funcionariosDAO.js
    
    * criado método insereFuncionario no arquivo funcionariosDao.js
    
    * criado método deletaFuncionario no arquivo funcionariosDAO.js
    
    Co-authored-by: Brenda Tondin <102706943+brendatondin@users.noreply.github.com>

--- src/DAO/funcionariosDAO.js
+++ src/DAO/funcionariosDAO.js
@@ -28,5 +28,38 @@ const funcionariosDAO = {
                     resolve(row)
             })
         })
+    },
+
+    insereFuncionario: (funcionario) => {
+        const INSERE_FUNCIONARIO = `
+        INSERT INTO FUNCIONARIOS (nome, email, cargo, salario, contato)
+        VALUES (?,?,?,?,?)
+        `
+        return new Promise((resolve, reject) => {
+            db.run(INSERE_FUNCIONARIO,
+                funcionario.nome, funcionario.email, funcionario.cargo, funcionario.salario, funcionario.contato,
+                (error) => {
+                    if (error)
+                        reject(error)
+                    else
+                        resolve(funcionario)
+                }
+            )
+        })
+    },
+
+    deletaFuncionario: (id) => {
+        const DELETA_FUNCIONARIO = `
+        DELETE FROM FUNCIONARIOS
+        WHERE id = ?
+        `
+        return new Promise((resolve, reject) => {
+            db.get(DELETA_FUNCIONARIO, id, (error, row) => {
+                if (error)
+                    reject(error)
+                else
+                    resolve(row)
+            })
+        })
     }
 }

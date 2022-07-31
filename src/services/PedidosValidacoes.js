394fd2dfd1b56212b5cb97dba84b2463069d9248
brendatondin <bentotbrenda@gmail.com>
2022-07-31 17:44:00 -0300

    fixed: bug de tratamento de erro da rota DELETE da entidade Pedidos, criada nova pasta de validacoes Pedidosalidacoe.js

--- src/services/PedidosValidacoes.js
+++ src/services/PedidosValidacoes.js
@@ -1,42 +1,7 @@
 import pedidosDAO from "../DAO/pedidosDAO.js" 
-const Validacoes = {
 
-    _validaGet : async (contato, callback)=>{
-        const cliente = await callback(contato)
-        if(cliente === undefined){
-            throw new Error (`Aviso: ${contato} não encontrado!`)
-        }else{
-            return cliente
-        }
-    },
 
-    _ValidaDeleta : async (id, callback)=>{
-        const cliente = await callback(id)
-        if(cliente == undefined){
-            throw new Error(`Aviso: ${id} não deletado!`)
-        }else{
-            return cliente
-        }
-    },
-
-    _ValidaAtualiza : async (id, callback, novoBody)=>{
-        const cliente = await callback(id, novoBody)
-            if(cliente === undefined){
-                throw new Error("Não conseguimos atualizar essa informação no banco de dados")
-            }else{
-                return cliente
-            }
-        
-    },
-
-    _ValidaReqBody : async (body)=>{
-        if(body.nome && body.email && body.contato){
-            return body
-        }else{
-            throw new Error ("Não foi possivel atualizar essa informação!")
-        }
-    },
-
+const PedidosValidacoes = {
     _validaGetPedidos : async (comanda, callback)=>{
         const pedidos = await callback(comanda)
         if(pedidos === undefined){
@@ -83,4 +48,4 @@ const Validacoes = {
     }
 }
 
-export default Validacoes
+export default PedidosValidacoes

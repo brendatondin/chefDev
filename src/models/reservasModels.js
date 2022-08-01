b0947dab83c2d95bb60df325799b5df5570ca3c5
Henry-Januario <102758431+Henry-Januario@users.noreply.github.com>
2022-08-01 18:26:54 -0300

    Henry reservas (#121)
    * Criei os arquivos da model e controler das reservas
    
    * Develop (#3)
    
    * teste de branch
    
    * alteração do nome das pastas para iniciar com letras minusculas
    
    * criado arquivo de configuração do banco de dados sqlite3
    
    * criada rotas de pedidos
    
    * apagado comentario no arquivo app.js  a pedido do guilherme
    
    Co-authored-by: GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
    Co-authored-by: Guilherme <102765157+Dev-DaMata@users.noreply.github.com>
    
    * servidor aberto e rota get respondendo
    
    * inserindo verificação de cargo = somente Maitre pode alterar o bd das reservas
    
    * Rota post criada e funcionando
    
    * adicionado controller das reservas no arquivo app.js
    
    * reservaModel alterada para funcional
    
    * Inserindo tratamento de erros try/catch  na rota get das reservas
    
    * atualizando rota post e inserindo tratamento de erros
    
    * Criando rota delete para reservas
    
    * Criando a Dao e adicionando metodos na model
    
    * banco de dados das reservas criado e populado
    
    * reservasController funcionando
    
    * Rota para pegar somente uma reserva funcionando
    
    * debugando e validando a rota post das reservas
    
    * rota delete criada
    
    * Retirado o comentário a pedido da gerencia
    
    * resolvendo conflito de arquivo do pedido
    
    * resolvendo conflito de banco de dados
    
    * rota delete e rota put criadas
    
    * Model, Controller, Dao e Validacoes da entidade Reserva foram refatoradas para se adequar com as regras de negócio
    
    Co-authored-by: Brenda Tondin <102706943+brendatondin@users.noreply.github.com>
    Co-authored-by: GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
    Co-authored-by: Guilherme <102765157+Dev-DaMata@users.noreply.github.com>

--- src/models/reservasModels.js
+++ src/models/reservasModels.js
@@ -8,17 +8,15 @@ const reservasModel = {
     verUmaReserva : async (idReserva)=>{
         return await reservasDAO.verUmaReserva(idReserva)
     },
-
     agendarReserva : async (reserva)=>{
         return await reservasDAO.agendarReserva(reserva)
     },
-
     deletaReserva : async (nomeCliente)=>{
         return await reservasDAO.deletaReserva(nomeCliente)
     },
-
-
-
+    atualizaReserva : async (idReserva, reservaAtualizada)=>{
+        return await reservasDAO.atualizaReserva(idReserva, reservaAtualizada)
+    }
 }
 
 export default reservasModel
